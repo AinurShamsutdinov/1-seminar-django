0cec57b4f95fcd69ae732742aa45adb482fb6853
Ainur Shamsutdinov <ainurshamsutdinov@Ainurs-MacBook-Pro.local>
2024-05-13 16:48:07 +0300

    6 getting back sqlite

--- seminar/seminar/settings.py
+++ seminar/seminar/settings.py
@@ -94,26 +94,26 @@ WSGI_APPLICATION = 'seminar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Ainur$seminar_django',
-        'USER': 'Ainur',
-        'PASSWORD': 'dbpassword',
-        'HOST': 'Ainur.mysql.pythonanywhere-services.com',
-        'OPTIONS': {
-            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
-    } 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Ainur$seminar_django',
+#         'USER': 'Ainur',
+#         'PASSWORD': 'dbpassword',
+#         'HOST': 'Ainur.mysql.pythonanywhere-services.com',
+#         'OPTIONS': {
+#             'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#         },
+#     } 
+# }
 
 
 # Password validation
